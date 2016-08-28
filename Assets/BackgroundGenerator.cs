0178f65df91ae449cafdbc9badd689b1e6c7d4ec
Adan Alonso <adan.alonso.s@gmail.com>
2016-08-28 12:58:34 +0100

    graficos, adornos aleatorios

--- Assets/BackgroundGenerator.cs
+++ Assets/BackgroundGenerator.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class BackgroundGenerator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public float waitBetweenObjects;
+
+	[System.Serializable]
+	public struct BackgroundObject
+	{
+		public GameObject prefab;
+		public Transform spawnAt;
+		public float probability;
+	};
+
+	public BackgroundObject cactus;
+	public BackgroundObject column;
+	public BackgroundObject bird;
+
+	void Start() {
+		StartCoroutine (spawn(cactus));
+		StartCoroutine (spawn(column));
+		StartCoroutine (spawn(bird));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	IEnumerator spawn(BackgroundObject obj) {
+		while (true) {
+			yield return new WaitForSeconds (waitBetweenObjects);
+			float random = Random.Range (0f, 1f);
+			if (random < obj.probability) {
+				Instantiate (obj.prefab, obj.spawnAt.position, Quaternion.identity);
+			}
+			yield return 0;
+		}
 	}
 }
